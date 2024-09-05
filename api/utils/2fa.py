c581ba0703a06b2ebb16a60d43eeb5191e0327d5
AJTimePyro <ajtimepyro@gmail.com>
2024-09-05 19:23:51 +0530

    improved 2fa functionality

--- api/utils/2fa.py
+++ api/utils/2fa.py
@@ -2,27 +2,29 @@ import smtplib
 import pyotp
 from email.mime.text import MIMEText
 
-key = "base32secret3232"
-
+key = ""
 totp = pyotp.TOTP(key)
-otp_value = totp.now()
-print(otp_value)
+SENDER_EMAIL = ""
+SENDER_PASSWORD = ""
 
-user_email = 'raghavagg2005@gmail.com'  # Replace with the user's email address
+def send_email(receiver_email, msg):
+    try:
+        smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp_server.starttls()
+        smtp_server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        smtp_server.sendmail(SENDER_EMAIL, receiver_email, msg.as_string())
+        smtp_server.quit()
+    except Exception as e:
+        print(f"Error sending OTP: {e}")
 
-# Create the email message
-msg = MIMEText(f"Your OTP is: {otp_value}")
-msg['Subject'] = 'Your OTP for Login'
-msg['From'] = 'aggarwalraghav864@gmail.com'  # Replace with your email address
-msg['To'] = user_email
+def send_otp(receiver_email):
+    otp_value = totp.now()
 
-# Send the email
-try:
-    smtp_server = smtplib.SMTP('smtp.gmail.com', 587)  # Replace with your SMTP server details
-    smtp_server.starttls()
-    smtp_server.login('aggarwalraghav864@gmail.com', 'vlwy qwnq prhk hvlm')  # Replace with your email credentials
-    smtp_server.sendmail('aggarwalraghav864@gmail.com', [user_email], msg.as_string())
-    smtp_server.quit()
-    print(f"OTP sent to {user_email}")
-except Exception as e:
-    print(f"Error sending OTP: {e}")
+    msg = MIMEText(f"Your OTP is: {otp_value}")
+    msg['Subject'] = 'Your OTP for Login'
+    msg['From'] = SENDER_EMAIL
+    msg['To'] = receiver_email
+
+    send_email(receiver_email, msg)
+
+    return otp_value
