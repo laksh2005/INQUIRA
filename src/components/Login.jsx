8dc88a457ca39b61829a082ec5be4127b91815b6
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2024-09-06 19:27:58 +0530

    merge sorted

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,76 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Header from './Header';
 import axios from 'axios';
 
 const Login = () => {
-
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [otp, setOtp] = useState('');
   const [isSignUpForm, setIsSignUpForm] = useState(false);
+  const [isOTPForm, setIsOTPForm] = useState(false);
 
   const toggleSignUpForm = () => {
     setIsSignUpForm(!isSignUpForm);
-  }
+  };
 
   const loginHandler = async () => {
     if (!email || !password) {
       // TODO: Add error handling
+      return;
     }
 
     try {
-      const res = await axios.post('api/login', {
-        email,
-        password
-      })
+      const res = await axios.post('api/login', { email, password });
 
       if (res.status === 200) {
-        setIsOTPForm(true)
+        setIsOTPForm(true);
       }
-      console.log(res)
+      console.log(res);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const registerHandler = async () => {
     if (!username || !email || !password) {
       // TODO: Add error handling
+      return;
     }
 
     try {
       const res = await axios.post('api/register', {
         username,
         email,
-        password
-      })
+        password,
+      });
 
       if (res.status === 201) {
-        setIsOTPForm(true)
+        setIsOTPForm(true);
       }
-      console.log(res)
+      console.log(res);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const authHandler = () => {
     if (isSignUpForm) {
-      registerHandler()
+      registerHandler();
     } else {
-      loginHandler()
+      loginHandler();
     }
-  }
+  };
 
   const otpVerificationHandler = async () => {
     if (!otp) {
       // TODO: Add error handling
+      return;
     }
 
-    const apiEndpoint = isSignUpForm ? 'api/verify-email' : 'api/login/otp-verify'
-    const res = await axios.post(apiEndpoint, {
-      email,
-      otp
-    })
-    console.log(res)
-  }
+    const apiEndpoint = isSignUpForm ? 'api/verify-email' : 'api/login/otp-verify';
+    try {
+      const res = await axios.post(apiEndpoint, { email, otp });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
@@ -78,54 +83,72 @@ const Login = () => {
         <Header />
       </div>
 
-    <div className="bg-gradient-to-br  from-black to-gray-600 min-h-screen flex justify-center items-center">
-    <form
-      className="bg-gradient-to-tl  from-black to-gray-600 text-white rounded-lg bg-opacity-65 absolute p-12 w-4/12 mx-auto mt-[0] right-0 left-0"
-      onSubmit={(e) => e.preventDefault()}
-    >
-      <h1 className="font-bold text-3xl py-4">{isSignUpForm ? "Sign Up" : "Sign In"}</h1>
-      <p className="p-4 text-gray-300 hover:text-white cursor-pointer" onClick={toggleSignUpForm}>
-        {isSignUpForm ? "Already a user? Sign In now." : "New to GAIL Bot? Sign up now."}
-      </p>
-      
-      {isSignUpForm && (
-        <input
-          type="text"
-          placeholder="Name"
-          className="p-4 my-2 font-bold w-full bg-gray-700 hover:border-white bg-opacity-80 text-white"
-        />
-      )}
-      
-      <input
-        type="text"
-        placeholder="E-mail"
-        className="p-4 my-2 font-bold w-full bg-gray-700 bg-opacity-80 text-white"
-      />
+      <div className="bg-gray-900 min-h-screen flex justify-center items-center">
+        <form
+          className="bg-black text-white rounded-lg bg-opacity-65 absolute p-12 w-4/12 mx-auto mt-[0] right-0 left-0"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          {isOTPForm ? (
+            <>
+              <h1 className="font-bold text-3xl py-4">Enter OTP</h1>
+              <input
+                type="text"
+                placeholder="Enter OTP"
+                className="p-4 my-2 font-bold w-full bg-gray-700 hover:border-white bg-opacity-80 text-white"
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+              />
+              <button
+                className="p-4 my-6 text-black font-bold bg-gray-200 hover:bg-gray-500 hover:text-white w-full rounded-lg hover:cursor-pointer"
+                onClick={otpVerificationHandler}
+              >
+                Submit
+              </button>
+            </>
+          ) : (
+            <>
+            <h1 className="font-bold text-3xl py-4 bg-gradient-to-l from-custom1 to-custom2 text-transparent bg-clip-text">{isSignUpForm ? "Sign Up" : "Sign In"}</h1>
+            <p className="p-4 text-gray-300 hover:text-white cursor-pointer" onClick={toggleSignUpForm}>
+              {isSignUpForm ? "Already a user? Sign In now." : "New to GAIL Bot? Sign up now."}
+            </p>
 
-      {isSignUpForm && (
-        <input
-          type="text"
-          placeholder="Enter Phone Number"
-          className="p-4 my-2 font-bold w-full bg-gray-700 hover:border-white bg-opacity-80 text-white"
-        />
-      )}
-      
-      <input
-        type="password"
-        placeholder="Enter Password"
-        className="p-4 my-2 font-bold w-full bg-gray-700 hover:border-white bg-opacity-80 text-white"
-      />
-      
-      <button
-        className="p-4 my-6 text-black font-bold bg-gray-200 hover:bg-gray-500 hover:text-white w-full rounded-lg hover:cursor-pointer"
-      >
-        {isSignUpForm ? "Sign Up" : "Sign In"}
-      </button>
-      
-    </form>
-  </div>
-  </div>
-  )
-}
+              {isSignUpForm && (
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="p-4 my-2 font-bold w-full bg-gray-700 hover:border-white bg-opacity-80 text-white"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              )}
 
-export default Login
+              <input
+                type="email"
+                placeholder="E-mail"
+                className="p-4 my-2 font-bold w-full bg-gray-700 bg-opacity-80 text-white"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <input
+                type="password"
+                placeholder="Enter Password"
+                className="p-4 my-2 font-bold w-full bg-gray-700 hover:border-white bg-opacity-80 text-white"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button
+              className="p-4 my-6 text-black font-bold bg-gradient-to-r from-custom1 to-custom2 hover:bg-gradient-to-l from-custom1 to-custom2 text-white text-lg py-3 px-8 mt-3 transition duration-300 ease-in-out hover:text-white hover:border-white border border-transparent w-full"
+              >
+                {isSignUpForm ? "Sign Up" : "Sign In"}
+              </button>
+            </>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
