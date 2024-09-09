426727845d0edbc512d0e02838dc38c6fee059b6
Raghavaggarwal2 <raghavagg2005@gmail.com>
2024-09-09 22:17:27 +0530

    Subscribe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Body from './components/Body';
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/subscribe")
+    .then(res => res.json())
+    .then(data => setData(data.message))
+    .catch(err => console.log(err));
+  });
+
   return (
     <Body />
   );
